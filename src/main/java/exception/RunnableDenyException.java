af798db12b0191bccfec56d2a00f589dca7b49c0
陶清晨 <776404387@qq.com>
2022-02-15 10:32:52 +0800

    #线程池

--- src/main/java/exception/RunnableDenyException.java
+++ src/main/java/exception/RunnableDenyException.java
@@ -6,7 +6,7 @@ package exception;
  * @Date 2021/1/11 19:42
  * @Version 1.0
  */
-
+//选择拒绝策略为 抛错时，抛出该异常
 public class RunnableDenyException extends RuntimeException {
 
     public RunnableDenyException(String message){
